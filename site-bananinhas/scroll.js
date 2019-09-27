e3e4a9d585e679ace920a6446af9df26fb7ede95
labcaxias <labcaxias@CDCLAB_15.labdc.zentyal>
2019-09-27 08:29:08 -0300

    Atualizando o menu

--- site-bananinhas/scroll.js
+++ site-bananinhas/scroll.js
@@ -1,11 +1,16 @@
 
-var navpos = $('section.nav-alto').offset();
-    console.log(navpos.top);
-    $(window).bind('scroll', function() {
-      if ($(window).scrollTop() > navpos.top) {
-        $('section.nav-alto').addClass('fixed');
-       }
-       else {
-         $('section.nav-alto').removeClass('fixed');
-       }
-    });
+var height = $('#header').height();
+
+$(window).scroll(function () {
+	if($(this).scrollTop() > height){
+		$('.nav-alto').addClass('fixed');
+	}else{
+		$('.nav-alto').removeClass('fixed');
+	}
+});
+
+
+
+    $('.pequeno').waypoint(function(direction) {
+    	$('.conteudocinza').addClass('animated fadeIn');
+    })
