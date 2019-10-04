a4815ddc93420bd44fd3ffe80347738a6a47612f
UilsonGabriel <33969847+UilsonGabriel@users.noreply.github.com>
2019-10-03 23:01:57 -0300

    pagina do usuario_incompleto

---
+++ logincadastro/usuarioPage.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<link href='style.css' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+<header>
+<div id="topdiv" class="text"><h1>Olá @nome!</h1><h1>Bem vindo à área do usuário!</h1></div>
+<div id="img_perfil"><img id="img_perfil" src=""></div>
+
+</header>
+
+<div id="menubar">
+	<nav class="menu">
+	<ul>
+		<li><a href="">Alterar Email</a></li>
+		<li><a href="">Alterar Senha</a></li>
+		<li><a href="">Cadastrar Receitas</a></li>
+		<li><a href="">Cadastrar Ingredientes</a></li>
+		<li><a href="">Minhas Receitas</a></li>
+		<li><a href="">Meus Ingredientes</a></li>
+	<ul>
+	</nav>
+</div>
+
+</body>
+
+</html>
