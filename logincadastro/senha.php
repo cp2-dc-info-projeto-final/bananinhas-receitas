2b64bd2d16708ca59db3f8c648e5dc09bb7183b2
UilsonGabriel <33969847+UilsonGabriel@users.noreply.github.com>
2019-10-10 20:42:44 -0300

    senha completo

---
+++ logincadastro/senha.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<link href='email.css' rel='stylesheet' type='text/css' />
+</head>
+
+<body>
+
+<div id="leftside" ></div>
+
+<div id="rightside">
+	<a href="" ><button id="user_area"><p>Home</p></button></a>
+	<a href="usuariopage.php" ><button id="user_area"><p>Área do Usuário</p></button></a>
+	<!-- Fazer o sql connection da trocar de email-->
+	<form  action="###.php" method="post"> 	
+		<div class="form-group">
+			<p>Senha Atual:</p>
+			<input name="senha1" type="password" id="senha1" placeholder="Senha Atual">
+		</div>
+		<div class="form-group">
+			<p>Senha Nova:</p>
+			<input name="senha2" type="password" id="senha2" placeholder="Senha Nova">
+		</div>  
+			<button type="submit" id="enviar">ALTERAR SENHA</button>
+	</form>	
+</div>
+
+	
+
+</body>
+
+</html>
