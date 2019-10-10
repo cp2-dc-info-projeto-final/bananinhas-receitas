977789faa6c6dc64e25cfa98016aae7726ec8c1b
UilsonGabriel <33969847+UilsonGabriel@users.noreply.github.com>
2019-10-10 17:40:33 -0300

    userpage incompleto

--- logincadastro/usuarioPage.php
+++ logincadastro/usuarioPage.php
@@ -1,29 +1,45 @@
 <html>
 <head>
-<link href='style.css' rel='stylesheet' type='text/css' />
-<script src="scroll2.js"></script>
+<link href='userpage.css' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
 
-<header>
-<div id="topdiv" class="text"><h1>Olá @nome!</h1><h1>Bem vindo à área do usuário!</h1></div>
-
-
+<header>	
+	<div id="img_banana">
+		<img align="center" src="Imagens\bananalogo.png" />
+	</div>
+	
+	<div class ="logado">
+			<p>Olá @nome!</p>
+			<p>Bem vindo à área do usuário!</p>
+	</div>
+	
 </header>
 
-<div id="menubar">
+
 	<nav class="menu">
-	<ul>
-		<li><a href="">Alterar Email</a></li>
-		<li><a href="">Alterar Senha</a></li>
-		<li><a href="">Cadastrar Receitas</a></li>
-		<li><a href="">Cadastrar Ingredientes</a></li>
-		<li><a href="">Minhas Receitas</a></li>
-		<li><a href="">Meus Ingredientes</a></li>
-	<ul>
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Alterar Email</a></li>
+			<li><a href="">Alterar Senha</a></li>
+			<li><a href="">Cadastrar Receitas</a></li>
+			<li><a href="">Cadastrar Ingredientes</a></li>
+			<li><a href="">Minhas Receitas</a></li>
+			<li><a href="">Meus Ingredientes</a></li>
+		</ul>
 	</nav>
+
+
+<div class="folder">
+	<img src="" />
 </div>
+
+<div class="folder">
+	<img src="" />
+</div>
+
+
 
 </body>
 
