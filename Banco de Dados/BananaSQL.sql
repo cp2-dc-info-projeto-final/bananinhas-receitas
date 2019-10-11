15391f3db3308c3e766ef09d356a3db17c4cffb1
= <leticiabarroso4@gmail.com>
2019-10-11 10:20:22 -0300

    Corrigindo banco de dados
    Mudou entidades e criou tabela agregadora: receitaingrediente

--- Banco de Dados/BananaSQL.sql
+++ Banco de Dados/BananaSQL.sql
@@ -2,7 +2,7 @@ use bananaSQL;
 
 create table cliente(
     id int primary key,
-    nome varchar(30),
+    nome varchar(50),
     email varchar(30),
     tel int,
     senha varchar(255),
@@ -11,26 +11,33 @@ create table cliente(
 );
 
 create table ingredientes(
-    idingredientes int primary key,
-    Nome varchar(30)
+    id int primary key,
+    Nome varchar(50)
 );
 
 create table categoria(
     idreceitas int primary key,
-    Nome varchar(30)
+    Nome varchar(50)
 );
 
 create table receitas(
     idreceitas int primary key,
-    nome varchar(30),
+    nome varchar(50),
     descricao varchar(200),
     mododepreparo varchar(1000),
     ultimaatualizacao date,
     nota int,
     idcliente int,
-    idingredientes int,
-    CONSTRAINT fk_receiclientes FOREIGN KEY (idcliente) REFERENCES idcliente(cliente),
-    CONSTRAINT fk_receingrediente FOREIGN KEY (idingredientes) REFERENCES id(ingredientes)
+    CONSTRAINT fk_receiclientes FOREIGN KEY (idcliente) REFERENCES idcliente(cliente)
+);
+
+create table receitaingrediente(
+    idreceita int,
+    idingrediente int,
+    quantingr int,
+    escalausada varchar(30),
+    CONSTRAINT fk_receitasing FOREIGN KEY (idreceita) REFERENCES id(receitas),
+    CONSTRAINT fk_ingredientesreceita FOREIGN KEY (idingrediente) REFERENCES id(ingredientes)
 );
 
 create table favoritos(
