0f6b47b7dbc4a81d3f5e0538fa95c483b013fd2a
Felipe-S-Lysgd <fsiqesp2@gmail.com>
2019-10-23 16:46:27 -0300

    ajuste no mvc da pesquisa de ingrediente

--- paginainicial/pesquisa.php
+++ paginainicial/pesquisa.php
@@ -1,6 +1,10 @@
 <?php
 require "pesquisacon.php";
 
+$pesquisar = $_POST['pesquisar'];
+$result  = "SELECT id, Nome FROM ingredientes WHERE nome LIKE '%$pesquisar%' LIMIT 5";
+$resultado = mysqli_query($conn, $result);
+
 while($rows_ingredientes = mysqli_fetch_array($resultado)){
       echo "ingrediente: ".$rows_ingredientes['Nome']."<br>";
       echo "id do ingrediente: ".$rows_ingredientes['id']."<br>";
--- paginainicial/pesquisacon.php
+++ paginainicial/pesquisacon.php
@@ -1,12 +1,12 @@
 <?php
-$servidor = "localhost";
-    $usuario = "root";
-    $senha = "";
-    $dbname = "BananaSQL";
+$servername = "localhost";
+$database = "bananasql";
+$username = "root";
+$password = "";
 
-    $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-    
-    $pesquisar = $_POST['pesquisar'];
-    $result  = "SELECT id, Nome FROM ingredientes WHERE nome LIKE '%$pesquisar%' LIMIT 5";
-    $resultado = mysqli_query($conn, $result);
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed:" . mysqli_connect_error());
+}
     ?>
