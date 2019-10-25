0caeec99bce4d45bdf69c32948933cdd8c48b19d
Felipe-S-Lysgd <fsiqesp2@gmail.com>
2019-10-25 10:41:49 -0300

    pesquisa de ingrediente dentro do cadastro de receitas

--- receitas/receitas.php
+++ receitas/receitas.php
@@ -2,6 +2,7 @@
         session_start();
     ?>
 <!DOCTYPE html>
+
 <html lang="pt-br">
 <head>
     <meta charset= "utf-8">
@@ -10,11 +11,18 @@
 <body>
 <div class="container">
 <form id="contact" action="receitasControle.php" method="post">
+
 <input type="text" name="nome" id="nomereceita" placeholder="Nome da receita" required="required">
 <input type="text" name="descricao" id="descricao" placeholder="descricao" required="required">
 <input type="text" name="mododepreparo" id="mododepreparo" placeholder="modo de preparo" required="required">
 <button type="submit" id="cadastrar">Cadastrar</button>
+
 </form>
 </div>
+
+<iframe src="formpesquisa.php" width="820" marginwidth="0" height="360" marginheight="0" scrolling="no" frameborder="0" hspace="0" vspace="0" allowtransparency="true" aplication="true"></iframe>
+
+<!-- https://www.youtube.com/watch?v=NN0hGn2Vbu4 assistir e tentar tirar o iframe -->
+
 </body>
-</html>
+</html>              
