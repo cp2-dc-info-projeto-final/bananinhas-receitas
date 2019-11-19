9d55fa41202927e63ca49ef70dd678d5ed6798b8
Felipe-S-Lysgd <fsiqesp2@gmail.com>
2019-11-19 18:39:36 -0300

    fazendo a pagina de receita incompleto

--- cadastroreceitas/pesquisa.php
+++ cadastroreceitas/pesquisa.php
@@ -1,14 +1,18 @@
 <?php
 require "pesquisacon.php";
+if (session_status() !== true){
+  session_start();
+}
 
 $pesquisar = $_POST['pesquisar'];
-$result  = "SELECT id, Nome FROM receitas WHERE ingredientes LIKE '%$pesquisar%' LIMIT 10";
-$resultado = mysqli_query($conn, $result);
+$sql  = "SELECT idreceitas, nome FROM receitas WHERE ingredientes LIKE '%$pesquisar%' LIMIT 10";
+$resultado = mysqli_query($conn, $sql);
 
-while($rows_receitas = mysqli_fetch_array($result)){
-      echo "receita: ".$rows_receitas['Nome']."<br>";
-      echo "id da receita: ".$rows_receitas['id']."<br>";
-      echo "<a href=\"\" action=\"addreceitas(" . $rows_receitas['id'] . ",'" . $rows_receitas['Nome'] . "')\"> Adicionar </a><br>";
+while($rows_receitas = mysqli_fetch_array($resultado)){
+      echo "receita: ".$rows_receitas['nome']."<br>";
+      echo "id da receita: ".$rows_receitas['idreceitas']."<br>";
+      echo "<a href=paginareceita.php action=\"addreceitas(" . $rows_receitas['idreceitas'] . ",'" . $rows_receitas['nome'] . "')\"> escolher receita </a><br>";
+      $_SESSION['id'] = $rows_receitas['idreceitas'];
     }
     require "formpesquisa.php"
 ?>
--- logincadastro/cadastroControle.php
+++ logincadastro/cadastroControle.php
@@ -21,7 +21,7 @@
     
     if (cadastro($nome,$email,$tel,$datanasc,$hash,$sexo)) {
         session_unset();
-        header("Location: usuarioPage.php");
+        header("Location: LoginCasdastro.php");
         exit();
     } else {
         $erro = "E-mail indisponível";
