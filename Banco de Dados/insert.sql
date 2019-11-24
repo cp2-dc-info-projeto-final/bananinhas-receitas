ab83bfdfdd2c6f04b393ea0ab45659f6640706e3
UilsonGabriel <uilson.gabriel2002@gmail.com>
2019-11-24 20:56:19 -0200

    inserts

---
+++ Banco de Dados/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO cliente(id,nome,email,tel,senha,sexo,datanasc)VALUES
+(1,'Felipe Siqueira','lysgd@gmail.com','987654321','$2y$10$nbaF.iB1yhMmEyvDEFt4N.sonCkiQGttLbEpDYJ29eo16kBd.oG3e','masculino','2001-02-17'),
+(2,'Uilson Castro','uilson@gmail.com','987654321','$2y$10$nbaF.iB1yhMmEyvDEFt4N.sonCkiQGttLbEpDYJ29eo16kBd.oG3e','masculino','2002-04-16'),
+(3,'Matheus','matheus@gmail.com','987654321','$2y$10$nbaF.iB1yhMmEyvDEFt4N.sonCkiQGttLbEpDYJ29eo16kBd.oG3e','masculino','2001-12-21'),
+(4,'Thaynan','thaynan@gmail.com','987654321','$2y$10$nbaF.iB1yhMmEyvDEFt4N.sonCkiQGttLbEpDYJ29eo16kBd.oG3e','masculino','2002-06-15'),
+
+
+
+
+
+
+
+INSERT INTO receitas (idreceitas,nome,descricao,Modpreparo,ingredientes) VALUES 
+('1','Bolo de banana','bolo feito de banana', 'mistura o bolo com as bananas',' bolo, banana'),
+('2','Banana com açucar','Banana misturada com açucar', 'mistura o açucar com as bananas','banana, açucar'),
+('3','Banana frita','deliciosa banana frita','frite a banana no oléo' ,'banana, açucar'),
+('4','Banana ensopada','deliciosa banana ensopada','cozinhe a banana na água quente ' ,'banana, água'),
+('5','Banana com sorvete','deliciosa banana com sorvete','coloque banana no sorvete' ,'banana, sorvete'),
+
