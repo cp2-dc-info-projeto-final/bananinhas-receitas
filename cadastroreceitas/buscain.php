27877e08b60730574009ff94c136ceccd373ee1f
UilsonGabriel <uilson.gabriel2002@gmail.com>
2019-11-24 21:28:31 -0200

    html e css busca ingredientes

--- cadastroreceitas/buscain.php
+++ cadastroreceitas/buscain.php
@@ -3,10 +3,22 @@ if (session_status() == false) {
     session_start();
     echo"bla bla bla";
 }
-?>
+?> 
 <html>
-    <form method="post" action="buscainrows.php">
-        pesquisar receita pelo ingrediente:<input type="text" id="pesquisar" name="pesquisar">
-        <input type="submit" value="buscar">
-    </form>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+
+ <body>
+    <form method="POST" action="buscainrows.php">
+  <div class="buscar-caixa">
+ 
+   <input type="text" name="pesquisar" class="buscar-txt" placeholder="Buscar..."/>
+   <button type="submit">
+   <a class="buscar-btn">
+    <i class="fas fa-search"></i>
+
+   </a>
+  </div>
+  </form>
+ </body>
 </html>
