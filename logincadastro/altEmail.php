e0bb63eada466c3d6d39f1496e0f413f3a20291f
UilsonGabriel <uilson.gabriel2002@gmail.com>
2019-11-24 12:52:51 -0200

    alt email funcionando

--- logincadastro/altEmail.php
+++ logincadastro/altEmail.php
@@ -1,32 +1,43 @@
 <?php
-   require "email.php";
-   session_start();
-	$conexao = mysql_connect('localhost', 'root', '', 'bananaSQL');
+	session_start();
+	$email = $_SESSION['email'];
+	$conexao = mysqli_connect('localhost', 'root', '', 'bananaSQL');		
+	
+	$sql = "SELECT id,senha FROM cliente WHERE email = '$email' ";
+	$date = mysqli_query($conexao, $sql);
+	$resultados = mysqli_fetch_array($date);
 
 
-    $email = $_POST["email"];
+
+    $email1 = $_POST["email"];
 	$email2 = $_POST["email2"];
 	$senha = $_POST["senha"]; 
-	$rash = mysqli_query($conexao, "SELECT senha FROM cliente");
-
+	$hash = $resultados['senha'];
+	
+if($email == $email1){
     if($email1 == "" || $email1 == null){
         $msg = "Insira um novo email válido";
         }
 		
-/*
-	if(password_verify($senha, $hash)== false;){
+
+	if(!password_verify($senha, $hash)){
 		$msg = "Digite a Senha corretamente";
 	}	
-*/  
-	if ($email == $email2){
+
+	if ($email1 == $email2){
 		$msg = "Digite um email novo";        
 	}
-	if($email != $email2 && password_verify($senha, $hash)){
+	if($email1 != $email2 && password_verify($senha, $hash)){
         $sql = "UPDATE cliente
-            SET email = '$email' where email = '$email' ";			
-		msg('Senha alterada com sucesso!');
+            SET email = '$email2' where email = '$email' ";	
+		mysqli_query($conexao, $sql);
+		$_SESSION['email'] = $email2;		
+		$msg = 'Email alterado com sucesso!';
         header("Location: usuarioPage.php");        
         exit();
     }
-
+}
+else{
+	$msg = 'Insira o email da sessão';
+}
 ?>
