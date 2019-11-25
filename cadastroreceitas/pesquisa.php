471cff5d598ffb5e8842b8847c4c696f639218b4
UilsonGabriel <uilson.gabriel2002@gmail.com>
2019-11-25 01:02:26 -0200

    psq incompleto

--- cadastroreceitas/pesquisa.php
+++ cadastroreceitas/pesquisa.php
@@ -3,20 +3,51 @@ require "pesquisacon.php";
 if (session_status() !== true){
   session_start();
 }
-
+ 
 $pesquisar = $_POST['pesquisar'];
 $sql  = "SELECT idreceitas, nome, descricao, ModPreparo, ingredientes FROM receitas WHERE nome LIKE '%$pesquisar%' LIMIT 10";
 $resultado = mysqli_query($conn, $sql);
 
+
 while($rows_receitas = mysqli_fetch_array($resultado)){
-      echo "receita: ".$rows_receitas['nome']."<br>";
-      echo "id da receita: ".$rows_receitas['idreceitas']."<br>";
-      echo "<a href=paginareceita.php action=\"addreceitas(" . $rows_receitas['idreceitas'] . ",'" . $rows_receitas['nome'] . "')\"> escolher receita </a><br>";
       $_SESSION['id'] = $rows_receitas['idreceitas'];
       $_SESSION['nome'] = $rows_receitas['nome'];
       $_SESSION['descricao'] = $rows_receitas['descricao'];
       $_SESSION['modpreparo'] = $rows_receitas['ModPreparo'];
       $_SESSION['ingredientes'] = $rows_receitas['ingredientes'];
     }
-    
 ?>
+
+
+<html>
+	<style>
+	body{
+		background: orange;
+	}
+	.receitas {
+		background: yellow;
+		width: 50%;
+		height: 100%;
+	}
+	
+	</style>
+	
+	<body>
+		<div class="receitas" >
+			<label>Receita: <?php 	echo $_SESSION['nome']."<br>";	?></label>
+			<button><?php	echo "<a href=paginareceita.php action=\"addreceitas("
+			. $rows_receitas['idreceitas'] . ",'" . $_SESSION['nome'] . "')\"> escolher receita </a><br>";?></button>
+		</div>
+	</body>
+</html>
+
+
+   
+	
+	
+	
+	
+	
+	
+	
+	
