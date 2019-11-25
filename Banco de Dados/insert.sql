7d367f96e1a37d1f1b1f942809bf35dc9a3bb8a0
UilsonGabriel <uilson.gabriel2002@gmail.com>
2019-11-25 02:31:17 -0200

    commit ae

--- Banco de Dados/insert.sql
+++ Banco de Dados/insert.sql
@@ -5,15 +5,11 @@ INSERT INTO cliente(id,nome,email,tel,senha,sexo,datanasc)VALUES
 (4,'Thaynan','thaynan@gmail.com','987654321','$2y$10$nbaF.iB1yhMmEyvDEFt4N.sonCkiQGttLbEpDYJ29eo16kBd.oG3e','masculino','2002-06-15'),
 
 
+INSERT INTO receitas (nome,descricao,Modpreparo,ingredientes) VALUES 
+('Bolo de banana','bolo feito de banana', 'mistura o bolo com as bananas',' bolo, banana'),
+('Banana com açucar','Banana misturada com açucar', 'mistura o açucar com as bananas','banana, açucar'),
+('Banana frita','deliciosa banana frita','frite a banana no oléo' ,'banana, açucar'),
+('Banana ensopada','deliciosa banana ensopada','cozinhe a banana na água quente ' ,'banana, água'),
+('Banana com sorvete','deliciosa banana com sorvete','coloque banana no sorvete' ,'banana, sorvete')
 
-
-
-
-
-INSERT INTO receitas (idreceitas,nome,descricao,Modpreparo,ingredientes) VALUES 
-('1','Bolo de banana','bolo feito de banana', 'mistura o bolo com as bananas',' bolo, banana'),
-('2','Banana com açucar','Banana misturada com açucar', 'mistura o açucar com as bananas','banana, açucar'),
-('3','Banana frita','deliciosa banana frita','frite a banana no oléo' ,'banana, açucar'),
-('4','Banana ensopada','deliciosa banana ensopada','cozinhe a banana na água quente ' ,'banana, água'),
-('5','Banana com sorvete','deliciosa banana com sorvete','coloque banana no sorvete' ,'banana, sorvete'),
 
--- cadastroreceitas/pesquisa.php
+++ cadastroreceitas/pesquisa.php
@@ -3,43 +3,28 @@ require "pesquisacon.php";
 if (session_status() !== true){
   session_start();
 }
- 
 $pesquisar = $_POST['pesquisar'];
-$sql  = "SELECT idreceitas, nome, descricao, ModPreparo, ingredientes FROM receitas WHERE nome LIKE '%$pesquisar%' LIMIT 10";
+$sql  = "SELECT idreceitas, nome, descricao, ModPreparo, ingredientes FROM receitas WHERE ingredientes LIKE '%$pesquisar%' LIMIT 10";
 $resultado = mysqli_query($conn, $sql);
-
-
+$num = 0;
 while($rows_receitas = mysqli_fetch_array($resultado)){
+	  echo "receita: ".$rows_receitas['nome']."<br>";
+      echo "id da receita: ".$rows_receitas['idreceitas']."<br>";
+      echo "<a href=paginareceita.php action=\"addreceitas(" . $rows_receitas['idreceitas'] . ",'" . $rows_receitas['nome'] . "')\"> escolher receita </a><br>";
       $_SESSION['id'] = $rows_receitas['idreceitas'];
       $_SESSION['nome'] = $rows_receitas['nome'];
       $_SESSION['descricao'] = $rows_receitas['descricao'];
       $_SESSION['modpreparo'] = $rows_receitas['ModPreparo'];
       $_SESSION['ingredientes'] = $rows_receitas['ingredientes'];
+	  
+	  $num += ($num +1);
     }
+    
 ?>
 
 
-<html>
-	<style>
-	body{
-		background: orange;
-	}
-	.receitas {
-		background: yellow;
-		width: 50%;
-		height: 100%;
-	}
-	
-	</style>
-	
-	<body>
-		<div class="receitas" >
-			<label>Receita: <?php 	echo $_SESSION['nome']."<br>";	?></label>
-			<button><?php	echo "<a href=paginareceita.php action=\"addreceitas("
-			. $rows_receitas['idreceitas'] . ",'" . $_SESSION['nome'] . "')\"> escolher receita </a><br>";?></button>
-		</div>
-	</body>
-</html>
+
+
 
 
    
