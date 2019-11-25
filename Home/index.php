ded006d64b843abe853df109442701284565d9d8
MatheusPereiraDaSilva <43180755+MatheusPereiraDaSilva@users.noreply.github.com>
2019-11-25 18:23:42 -0300

    Adicionando biblioteca de bootstrap

---
+++ Home/index.php
@@ -0,0 +1,209 @@
+<?php
+
+include "../home/nav.php";
+include "../home/links.php";
+
+?>
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <title>Página inicial</title>
+  </head>
+  <body class="bg-light">
+
+
+
+
+<div class="submenu">
+<div class="text-light pt-3">
+           <div class="container">
+               <div class="row">
+                   <div class="col-md-4 pb-2">
+                       <div class="media">
+                            <i class="fas fa-play-circle mr-3 display-4" ></i>
+                            <div class="media-body text-light">
+                              <h5 class="mt-0 ">Pesquisa rápida</h5>
+                              Encontre inúmeras receitas através da pesquisa por ingredientes específicos.
+                            </div>
+                          </div>
+                   </div>
+
+                   <div class="col-md-4 pb-3">
+                        <div class="media">
+                             <i class="fas fa-ribbon mr-3 display-4" ></i>
+                             <div class="media-body text-light">
+                               <h5 class="mt-0 ">Faça parte da equipe</h5>
+                               Cadastre-se em nosso site e tenha acesso a funções exclusivas do site.
+                             </div>
+                           </div>
+                    </div>
+                   
+                   <div class="col-md-4 pb-3">
+                        <div class="media">
+                             <i class="fas fa-spinner mr-3 display-4" ></i>
+                             <div class="media-body text-light">
+                               <h5 class="mt-0">Interaja com o site</h5>
+                               Favorite as receitas que gostar e envie as receitas que você conhece.
+                             </div>
+                           </div>
+                    </div>              
+               </div>
+           </div>
+          </div>
+          </div>
+
+
+<div class="cor-aba-receitas">
+<div class="container pt-5">
+   <div class="row">
+      <div class="col-sm">
+  	     <h5 class="border-bottom pb-2 border-white"><i class="fas fa-star"></i> Receitas em destaque</h5>
+      </div>
+   </div>
+</div>
+
+
+<div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-12 rounded">
+                <div class="hover_area_1 hover_area">
+                    <div class="hover_img_1">
+                        <img src="http://preview.ryetheme.com/codecanyon/miktu/assets/img/1.jpg" class="img-fluid" alt="">
+                        <div class="hover_overlay_1"></div>
+                        <div class="hover_content_1">
+                            <h4>Know More About UI Design</h4>
+                            <p class="text-white"> Por: Bananinhas receitas </p>
+                            <ul>
+                        
+                                <li><a href="#"><i class="fas fa-search"></i></a></li>
+                                <li><a href="#"><i class="fas fa-heart"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="hover_area_1 hover_area">
+                    <div class="hover_img_1">
+                        <img src="http://preview.ryetheme.com/codecanyon/miktu/assets/img/1.jpg" class="img-fluid" alt="">
+                        <div class="hover_overlay_1"></div>
+                        <div class="hover_content_1">
+                            <h4>Know More About UI Design</h4>
+                            <p class="text-white"> Por: Bananinhas receitas </p>
+                            <ul>
+                     
+                              <li><a href="#"><i class="fas fa-search"></i></a></li>
+                              <li><a href="#"><i class="fas fa-heart"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="hover_area_1">
+                    <div class="hover_img_1">
+                        <img src="http://preview.ryetheme.com/codecanyon/miktu/assets/img/1.jpg" class="img-fluid" alt="">
+                        <div class="hover_overlay_1"></div>
+                        <div class="hover_content_1">
+                            <h4>Know More About UI Design</h4>
+                            <p class="text-white"> Por: Bananinhas receitas </p>
+                            <ul>
+                     
+                              <li><a href="#"><i class="fas fa-search"></i></a></li>
+                              <li><a href="#"><i class="fas fa-heart"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-12 rounded">
+                <div class="hover_area_1 hover_area">
+                    <div class="hover_img_1">
+                        <img src="http://preview.ryetheme.com/codecanyon/miktu/assets/img/1.jpg" class="img-fluid" alt="">
+                        <div class="hover_overlay_1"></div>
+                        <div class="hover_content_1">
+                            <h4>Know More About UI Design</h4>
+                            <p class="text-white"> Por: Bananinhas receitas </p>
+                            <ul>
+                             
+                                <li><a href="#"><i class="fas fa-search"></i></a></li>
+                                <li><a href="#"><i class="fas fa-heart"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="hover_area_1 hover_area">
+                    <div class="hover_img_1">
+                        <img src="http://preview.ryetheme.com/codecanyon/miktu/assets/img/1.jpg" class="img-fluid" alt="">
+                        <div class="hover_overlay_1"></div>
+                        <div class="hover_content_1">
+                            <h4>Know More About UI Design</h4>
+                            <p class="text-white"> Por: Bananinhas receitas </p>
+                            <ul>
+                             
+                              <li><a href="#"><i class="fas fa-search"></i></a></li>
+                              <li><a href="#"><i class="fas fa-heart"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="hover_area_1">
+                    <div class="hover_img_1">
+                        <img src="http://preview.ryetheme.com/codecanyon/miktu/assets/img/1.jpg" class="img-fluid" alt="">
+                        <div class="hover_overlay_1"></div>
+                        <div class="hover_content_1">
+                            <h4>Know More About UI Design</h4>
+                            <p class="text-white"> Por: Bananinhas receitas </p>
+                            <ul>
+                              
+                              <li><a href="#"><i class="fas fa-search"></i></a></li>
+                              <li><a href="#"><i class="fas fa-heart"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+    </div>
+
+
+<div class="container">
+	<div class="row pb-5">
+      <div class="col-sm text-center">
+  	     <a href="#" class="btn btn-info btn-lg">
+          <i class="fas fa-search"></i> Ver mais 
+        </a>
+      </div>
+   </div>
+</div>
+
+<div class="border-bottom">
+</div>
+</div>
+
+
+<?php
+
+include "../home/footer.php";
+
+?>
+  </body>
+  </html>
