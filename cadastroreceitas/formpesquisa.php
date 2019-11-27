3c54fae4273048f6650097152c54cb8e47b43b4f
MatheusPereiraDaSilva <43180755+MatheusPereiraDaSilva@users.noreply.github.com>
2019-11-27 06:09:30 -0300

    Atualizando menu

--- cadastroreceitas/formpesquisa.php
+++ cadastroreceitas/formpesquisa.php
@@ -14,19 +14,39 @@ if (session_status() == false) {
      </head>
  <body>
  
-<div class="j1">
-<ul class="nav">
-<li><a href="#">Home</a></li>
-<li><a href="#">Pesquisa</a></li>
-<li><a href="#">CadastroReceitas</a></li>
-<li><a href="#">Sobre</a></li>
-</ul>
-</div>
+<div class="nav-alto">
+       <section class="nav">
+           <div class="container">
+               <div class="imgkj">
+                  
+               </div>
+               <div class="lista">
+                <nav class="menu">
+                  <ul>
+                    <li>
+                      <a href="..\..\cadastroreceitas\formpesquisa.php" title="Receitas"> Busca(Nome)</a>
+                    </li>
+                    <li>
+                      <a href="..\..\cadastroreceitas\buscain.php" title="Ingredientes">Busca(Ingredientes)</a>
+                    </li>
+                    <li>
+                      <a href="#conteudocinza4" title="Sobre"> Sobre nós </a>
+                    </li>
+                  </ul>
+                </nav>
+               </div>
+               <div class="logincadastro">
 
-
-<a href="http://127.0.0.1/edsa-bananinhas-receitas/logincadastro/home/home.html">
-<img src="../Imagens/logo.png" alt="" width="180" height="92" border="0">
-</a>
+               <a href="..\loginCasdastro.php" class="logincadastro" title="Cadastrologin">         
+               Logar ou Cadastrar
+               </a>
+         <a href="..\userorlogin.php" class="logincadastro" title="Cadastrologin">          
+               Área do Usuário
+               </a>
+               </div>
+           </div>
+       </section>
+    </div>
 
     <form method="POST" action="pesquisa.php" >
 	
