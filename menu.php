9110071e75a509eef684378e58f43fc04fb09dd1
UilsonGabriel <33969847+UilsonGabriel@users.noreply.github.com>
2019-12-26 13:45:52 -0300

    menu

---
+++ menu.php
@@ -0,0 +1,42 @@
+<html lang="pt-br">
+    <head>
+        <link rel="stylesheet" type="text/css" href="menu.css" \>
+    </head>
+
+
+<div id="wrapper">
+	<div class="nav-alto">
+       <section class="nav">
+           <div class="container">
+           	   <div class="imgkj">
+                  
+           	   </div>
+           	   <div class="lista">
+           	   	<nav class="menu">
+           	   		<ul>
+                          <li>
+           	   				<a href="#conteudocinza4" title="Sobre"> Home </a>
+           	   			</li>
+           	   			<li>
+           	   				<a href="..\..\cadastroreceitas\formpesquisa.php" title="Receitas"> Busca(Nome)</a>
+           	   			</li>
+           	   			<li>
+           	   				<a href="..\..\cadastroreceitas\buscain.php" title="Ingredientes">Busca(Ingredientes)</a>
+           	   			</li>          	   			
+           	   		</ul>
+           	   	</nav>
+           	   </div>
+           	   <div class="logincadastro">
+
+           	   <a href="..\loginCasdastro.php" class="logincadastro" title="Cadastrologin">    	   	
+           	   Logar ou Cadastrar
+           	   </a>
+			   <a href="..\userorlogin.php" class="logincadastro" title="Cadastrologin">    	   	
+           	   Área do Usuário
+           	   </a>
+           	   </div>
+           </div>
+       </section>
+    </div>
+
+<html>
