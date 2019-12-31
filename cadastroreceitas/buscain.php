a9e87a5df2b2eed68f89b7b3120d65852ed2a36b
MatheusPereiraDaSilva <43180755+MatheusPereiraDaSilva@users.noreply.github.com>
2019-12-31 13:43:08 -0300

    Atualizando páginas

--- cadastroreceitas/buscain.php
+++ cadastroreceitas/buscain.php
@@ -15,7 +15,7 @@ if (session_status() == false) {
      </head>
 
  <nav class="navbar navbar-expand-lg bg-primary">
-  <button class="navbar-toggler mb-2 navbar-dark" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 <!-- Menu dropdown -->
